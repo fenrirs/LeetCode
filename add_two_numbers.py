1c90c0bbbbfd6d65347b7bbbf3a2b2ad7159df6f
Fenrir Ni <fenrirs.x@gmail.com>
2014-09-07 20:57:28 +0800

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -16,20 +16,20 @@ Output: 7 -> 0 -> 8
 class Solution:
 	# @return a ListNode
 	def addTwoNumbers(self, l1, l2):
-    res = None
-    temp = res
-    tens = 0
-    while (l1!=None)or(l2!=None)or(tens!=0):
-      val = ((l1.val if l1!=None else 0)+(l2.val if l2!=None else 0))+tens
-      tens = val/10
-      val = val%10
-      if temp==None:
-        res = ListNode(val)
-        temp = res
-      else:
-        temp.next = ListNode(val)
-        temp = temp.next
-      l1 = l1.next if l1!=None else None
-      l2 = l2.next if l2!=None else None
-    return res
+		res = None
+ 		temp = res
+		tens = 0
+		while (l1!=None)or(l2!=None)or(tens!=0):
+			val = ((l1.val if l1!=None else 0)+(l2.val if l2!=None else 0))+tens
+			tens = val/10
+			val = val%10
+			if temp==None:
+				res = ListNode(val)
+				temp = res
+			else:
+				temp.next = ListNode(val)
+				temp = temp.next
+			l1 = l1.next if l1!=None else None
+			l2 = l2.next if l2!=None else None
+		return res
     
